5c460c41baa04cd0e88ed31bd712aade06a60234
VanessaLinaresSena <vanessalinaressena@gmail.com>
2026-03-09 13:22:01 -0500

    Initial commit

---
+++ src/config/titulo.js
@@ -0,0 +1,1 @@
+module.exports = 'Titulo del componente formativo'
---
+++ src/views/Actividad.vue
@@ -0,0 +1,435 @@
+<template lang="pug">
+  .curso-main-container.pb-3
+    BannerInterno(icono="far fa-question-circle" titulo="Actividad didáctica")
+    .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+  
+      #Actividad                
+        <Actividad :cuestionario="cuestionario"/>
+  
+  </template>
+
+<script>
+import Actividad from 'ecored-pkg-fliz/plugin/components/actividad/Actividad.vue'
+export default {
+  name: 'ActividadDidactica',
+  components: {
+    Actividad,
+  },
+  data: () => ({
+    cuestionario: {
+      tema: 'Microcontroladores y sensores',
+      titulo: 'Cuestionario',
+      introduccion:
+        '<b> Objetivo:</b> Evaluar la comprensión y el uso adecuado de las herramientas básicas en la ventana <em>Board</em> del <em>software</em> EAGLE.',
+      barajarPreguntas: true,
+      titulo_aprobado: '¡BUEN TRABAJO!.',
+      titulo_reprobado: 'VUELVA A INTENTARLO.',
+      preguntas: [
+        {
+          id: 1,
+          texto: 'PREGUNTA1',
+          imagen: require('@/assets/actividad/imagen1.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'OPCION1',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'OPCION2',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'OPCION3',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'OPCION4',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 2,
+          texto: 'PREGUNTA2',
+          imagen: require('@/assets/actividad/imagen1.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'OPCION1',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'OPCION2',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'OPCION3',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'OPCION4',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 3,
+          texto: 'PREGUNTA3',
+          imagen: require('@/assets/actividad/imagen1.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'OPCION1',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'OPCION2',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'OPCION3',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'OPCION4',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 4,
+          texto: 'PREGUNTA4',
+          imagen: require('@/assets/actividad/imagen1.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'OPCION1',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'OPCION2',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'OPCION3',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'OPCION4',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 5,
+          texto: 'PREGUNTA5',
+          imagen: require('@/assets/actividad/imagen2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'OPCION1',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'OPCION2',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'OPCION3',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'OPCION4',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 6,
+          texto: 'PREGUNTA6',
+          imagen: require('@/assets/actividad/imagen2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'OPCION1',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'OPCION2',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'OPCION3',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'OPCION4',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 7,
+          texto: 'PREGUNTA7',
+          imagen: require('@/assets/actividad/imagen2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'OPCION1',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'OPCION2',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'OPCION3',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'OPCION4',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 8,
+          texto: 'PREGUNTA8',
+          imagen: require('@/assets/actividad/imagen2.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'OPCION1',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'OPCION2',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'OPCION3',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'OPCION4',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 9,
+          texto: 'PREGUNTA9',
+          imagen: require('@/assets/actividad/imagen3.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'OPCION1',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'OPCION2',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'OPCION3',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'OPCION4',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 10,
+          texto: 'PREGUNTA10',
+          imagen: require('@/assets/actividad/imagen3.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'OPCION1',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'OPCION2',
+              esCorrecta: true,
+            },
+            {
+              id: 'c',
+              texto: 'OPCION3',
+              esCorrecta: false,
+            },
+            {
+              id: 'd',
+              texto: 'OPCION4',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 11,
+          texto: 'PREGUNTA11',
+          imagen: require('@/assets/actividad/imagen3.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Verdadero',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Falso',
+              esCorrecta: true,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 12,
+          texto: 'PREGUNTA12',
+          imagen: require('@/assets/actividad/imagen3.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Verdadero',
+              esCorrecta: true,
+            },
+            {
+              id: 'b',
+              texto: 'Falso',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 13,
+          texto: 'PREGUNTA13',
+          imagen: require('@/assets/actividad/imagen4.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Verdadero',
+              esCorrecta: true,
+            },
+            {
+              id: 'b',
+              texto: 'Falso',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 14,
+          texto: 'PREGUNTA14',
+          imagen: require('@/assets/actividad/imagen4.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Verdadero',
+              esCorrecta: false,
+            },
+            {
+              id: 'b',
+              texto: 'Falso',
+              esCorrecta: true,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+        {
+          id: 15,
+          texto: 'PREGUNTA15',
+          imagen: require('@/assets/actividad/imagen4.png'),
+          barajarRespuestas: true,
+          opciones: [
+            {
+              id: 'a',
+              texto: 'Verdadero',
+              esCorrecta: true,
+            },
+            {
+              id: 'b',
+              texto: 'Falso',
+              esCorrecta: false,
+            },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+      ],
+      mensaje_final_aprobado: '¡Excelente! Ha superado la actividad.',
+      mensaje_final_reprobado:
+        'Le recomendamos volver a revisar el componente formativo e intentar nuevamente la actividad didáctica.',
+    },
+  }),
+}
+</script>
